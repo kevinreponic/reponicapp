3ee187ca8bffef06ad87af62a21966df40c8a6e4
Kevin <kevin@reponic.org>
2019-05-13 18:53:22 -0400

    First Commit

---
+++ pages/SignUp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class SignUp extends Component {
+  render() {
+    return (
+      <View>
+        <Text> SignUp </Text>
+      </View>
+    )
+  }
+}
+
+export default SignUp
