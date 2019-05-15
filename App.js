fe2ec776880ad6d12645e2d534985f7b5c590353
Kevin <kevin@reponic.org>
2019-05-14 22:03:23 -0400

    RNFirebase integration

--- App.js
+++ App.js
@@ -8,42 +8,23 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { LoginScreen } from "./pages/Login";
+import { HomeScreen } from "./pages/Home";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const AppNavigator =  createStackNavigator({
+  Login : LoginScreen,
+  Home : HomeScreen
+},
+{
+  initialRouteName : "Login",
+  }
+);
 
+const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+  render(){
+    return <AppContainer />
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
--- pages/Login.js
+++ pages/Login.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
+import { Button, Input } from 'react-native-elements';
 
-export class Login extends Component {
+export class LoginScreen extends Component {
+  static navigationOptions = {
+    header: null
+  };
+
   render() {
     return (
-      <View>
-        <Text> Login </Text>
+      <View style={styles.container}>
+        <Text style={styles.welcome}> Reponic </Text>
+        <Button title="Home" onPress={()=>this.props.navigation.navigate('Home')} />
       </View>
     )
   }
 }
 
-export default Login
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    justifyContent : 'center',
+    alignItems : 'center',
+    backgroundColor : '#4286f4'
+  },
+
+  welcome : {
+    fontSize : 30,
+    textAlign : 'center',
+    margin : 10,
+    color : '#ffffff'
+  },
+})
+
+export default LoginScreen
